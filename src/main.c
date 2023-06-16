465bc8fffb71d3829cfc0a31e0aba3084de1a1ae
mal1kc <59576495+mal1kc@users.noreply.github.com>
2023-06-17 01:47:20 +0300

    repo init

---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include "print_hello.h"
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+  say_hi();
+
+  return EXIT_SUCCESS;
+}
---
+++ src/print_hello.c
@@ -0,0 +1,4 @@
+#include "print_hello.h"
+#include <stdio.h>
+
+void say_hi() { printf("hi,"); }
